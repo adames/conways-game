f7d5c6aaff9fb28ef8e520096af8b6cb1272ec07
adames <adames.hodelin@gmail.com>
2025-03-09 03:44:02 -0400

    added handler

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -1,34 +1,48 @@
 import { useState } from "react";
 
 const Grid = () => {
-  const width = 20;
-  const height = 20;
+  const row = 20;
+  const column = 20;
   const generateGrid = () => {
     const emptyGrid = [];
-    for (let i = 0; i < width; i++) {
-      emptyGrid.push(Array.from(Array(height), () => 0));
+    for (let i = 0; i < row; i++) {
+      emptyGrid.push(Array.from(Array(column), () => false));
     }
     return emptyGrid;
   };
   const [grid, setGrid] = useState(generateGrid);
 
+  const handleClick = (event) => {
+    const x = event.target.getAttribute("data-row");
+    const y = event.target.getAttribute("data-column");
+    console.log(`x = ${x}, y = ${y}`);
+    console.log(grid);
+    setGrid((previousGrid) => {
+      const gridCopy = previousGrid.map((row) => [...row]);
+      gridCopy[x][y] = !gridCopy[x][y];
+      return gridCopy;
+    });
+  };
 
   return (
     <div
       className="Grid"
       style={{
         display: "grid",
-        gridTemplateColumns: `repeat(${height}, 20px)`,
+        gridTemplateColumns: `repeat(${column}, 20px)`,
       }}
     >
-      {grid.map((width, widthIndex) =>
-        width.map((height, heightIndex) => (
+      {grid.map((row, rowIndex) =>
+        row.map((column, columnIndex) => (
           <button
-            key={`${widthIndex}-${heightIndex}`}
+            onClick={handleClick}
+            key={`${rowIndex}-${columnIndex}`}
+            data-row={rowIndex}
+            data-column={columnIndex}
             style={{
               width: 20,
               height: 20,
-              color: grid[widthIndex][heightIndex] ? "red" : "white",
+              color: grid[rowIndex][columnIndex] ? "black" : "white",
               border: "solid 1px black",
             }}
           />
